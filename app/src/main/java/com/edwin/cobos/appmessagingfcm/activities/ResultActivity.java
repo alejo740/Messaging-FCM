b2044d5cd118cf943d400126215c16ef25f88c7c
Edwin Alejandro Cobos Fonseca <alejo740@gmail.com>
2017-04-19 17:43:00 -0500

    Added custom notification and open new activity with data

--- app/src/main/java/com/edwin/cobos/appmessagingfcm/activities/ResultActivity.java
+++ app/src/main/java/com/edwin/cobos/appmessagingfcm/activities/ResultActivity.java
@@ -1,19 +1,28 @@
 package com.edwin.cobos.appmessagingfcm.activities;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.widget.TextView;
 
 import com.edwin.cobos.appmessagingfcm.R;
 
+import java.util.HashMap;
+
 public class ResultActivity extends AppCompatActivity {
 
     @Override
+    @SuppressWarnings("unchecked")
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_result);
 
         TextView messageTextView = (TextView) findViewById(R.id.messageTextView);
 
+        Intent intent = getIntent();
+        if(intent.getSerializableExtra("dataMap") instanceof HashMap) {
+            HashMap<String, String> hashMap = (HashMap<String, String>) intent.getSerializableExtra("dataMap");
+            messageTextView.setText(hashMap.get("customMessageKey"));
+        }
     }
 }
--- app/src/main/java/com/edwin/cobos/appmessagingfcm/services/FcmService.java
+++ app/src/main/java/com/edwin/cobos/appmessagingfcm/services/FcmService.java
@@ -5,9 +5,14 @@ import android.app.NotificationManager;
 import android.app.PendingIntent;
 import android.content.Context;
 import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.Color;
+import android.graphics.drawable.Drawable;
 import android.media.RingtoneManager;
 import android.net.Uri;
 import android.support.v4.app.NotificationCompat;
+import android.support.v4.content.res.ResourcesCompat;
 import android.util.Log;
 
 import com.edwin.cobos.appmessagingfcm.R;
@@ -15,7 +20,8 @@ import com.edwin.cobos.appmessagingfcm.activities.ResultActivity;
 import com.google.firebase.messaging.FirebaseMessagingService;
 import com.google.firebase.messaging.RemoteMessage;
 
-import static android.support.v4.app.NotificationCompat.PRIORITY_HIGH;
+import java.util.HashMap;
+import java.util.Map;
 
 /**
  * @author edwin.cobos
@@ -31,27 +37,39 @@ public class FcmService extends FirebaseMessagingService {
         //super.onMessageReceived(remoteMessage);
         Log.d(TAG, "From: " + remoteMessage.getFrom());
         Log.d(TAG, "Notification Message Body: " + remoteMessage.getNotification().getBody());
-        createNotification(remoteMessage.getNotification().getBody());
+        createNotification(remoteMessage);
     }
 
-    private void createNotification( String messageBody) {
-        Intent intent = new Intent( this , ResultActivity. class );
-        intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
-        PendingIntent resultIntent = PendingIntent.getActivity( this , 0, intent,
+    private void createNotification(RemoteMessage remoteMessage) {
+        String messageBody = remoteMessage.getNotification().getBody();
+
+        Map<String, String> map = remoteMessage.getData();
+        HashMap<String, String> hMap = (map instanceof HashMap)
+                                       ? (HashMap<String, String>) map
+                                       : new HashMap<>(map);
+
+        Intent intent = new Intent(this, ResultActivity.class);
+        intent.putExtra("dataMap", hMap);
+        intent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP | Intent.FLAG_ACTIVITY_SINGLE_TOP);
+
+        PendingIntent resultIntent = PendingIntent.getActivity(this, 0, intent,
                 PendingIntent.FLAG_ONE_SHOT);
 
+        Bitmap bmp = BitmapFactory.decodeResource(getResources(), R.mipmap.ic_notification_icon);
+
         Uri notificationSoundURI = RingtoneManager.getDefaultUri(RingtoneManager.TYPE_NOTIFICATION);
-        NotificationCompat.Builder mNotificationBuilder = new NotificationCompat.Builder( this)
-                .setSmallIcon(R.mipmap.ic_launcher)
-                .setContentTitle("Android Tutorial Point FCM Tutorial")
+        NotificationCompat.Builder mNotificationBuilder = new NotificationCompat.Builder(this)
+                .setSmallIcon(R.drawable.ic_security)
+                .setLargeIcon(bmp)
+                .setColor(Color.rgb(255, 72, 72))
+                .setContentTitle("FCM message")
                 .setPriority(Notification.PRIORITY_MAX)
                 .setContentText(messageBody)
-                .setAutoCancel( true )
+                .setAutoCancel(true)
                 .setSound(notificationSoundURI)
                 .setContentIntent(resultIntent);
 
-        NotificationManager notificationManager =
-                (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
+        NotificationManager notificationManager = (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
 
         notificationManager.notify(0, mNotificationBuilder.build());
     }
